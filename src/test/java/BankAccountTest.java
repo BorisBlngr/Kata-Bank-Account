cf3c92c5af2488261bdbd3ab3c3555c5a79bc14a
Boris <boris.thibaut.boulanger@gmail.com>
2019-04-26 15:15:57 +0200

    init

---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class BankAccountTest {
+
+    @Test
+    public void init() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
