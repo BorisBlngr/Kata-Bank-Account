571e0a675b96a694936f411ceacc92053aa10e2f
Boris <boris.thibaut.boulanger@gmail.com>
2019-04-26 16:13:12 +0200

    refactoring AccountTest & MoneyTest

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,31 +1,27 @@
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
     public void i_can_deposit_money_on_my_account() {
-        Account account = new Account(Money.of(50));
+        Account myAccount = new Account(Money.of(50));
 
-        account.deposit(Money.of(60));
+        myAccount.deposit(Money.of(60));
 
-        Account expected = new Account(Money.of(110));
-        Assertions.assertThat(account).isEqualTo(expected);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void money_can_not_be_negative() {
-        Money.of(-10);
+        Account expectedAccount = new Account(Money.of(110));
+        assertThat(myAccount).isEqualTo(expectedAccount);
     }
 
     @Test
-    public void i_can_retrieve_money_on_my_account() {
+    public void i_can_perform_a_partial_withdrawal_of_my_account() {
         Account myAccount = new Account(Money.of(60));
 
         myAccount.withdrawal(Money.of(20));
 
         Account expected = new Account(Money.of(40));
-        Assertions.assertThat(myAccount).isEqualTo(expected);
+        assertThat(myAccount).isEqualTo(expected);
     }
 
     @Test
@@ -35,8 +31,6 @@ public class AccountTest {
         myAccount.withdrawalFull();
 
         Account accountExpected = new Account(Money.of(0));
-
-        Assertions.assertThat(myAccount).isEqualTo(accountExpected);
-
+        assertThat(myAccount).isEqualTo(accountExpected);
     }
 }
---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class MoneyTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void money_can_not_be_negative() {
+        Money.of(-10);
+    }
+
+}
